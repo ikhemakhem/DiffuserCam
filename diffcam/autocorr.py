483496eecf6e544bcc53c58755323d9f69168a81
Adrien Schurger-Foy <adriensf@gmail.com>
2021-12-10 15:28:35 +0100

    attempt autocorr.py

--- diffcam/autocorr.py
+++ diffcam/autocorr.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from numpy.fft import fft2, ifft2
+from scipy.signal import correlate2d
 
 def autocorr2d(vals, pad_mode="reflect"):
     """
@@ -17,4 +18,29 @@ def autocorr2d(vals, pad_mode="reflect"):
     autocorr : py:class:`~numpy.ndarray`
     """
 
-    return np.ones_like(vals)
+    padded = np.pad(vals, ((int(vals.shape[0]/2), int(vals.shape[1]/2)),), mode=pad_mode)
+    # padded = vals
+    f = fft2(padded)
+    l2norm = np.absolute(f)**2
+    res = ifft2(l2norm)
+    
+
+    # return correlate2d(vals, vals, mode='same', boundary='fill')
+    return np.real(res)
+
+
+# def autocorrelation(x):
+#     xp = ifftshift((x - np.average(x))/np.std(x))
+#     n, = xp.shape
+#     xp = np.r_[xp[:n//2], np.zeros_like(xp), xp[n//2:]]
+#     f = fft(xp)
+#     p = np.absolute(f)**2
+#     pi = ifft(p)
+#     return np.real(pi)[:n//2]/(np.arange(n//2)[::-1]+n//2)
+
+# def autocorrelation(x) :
+#     # xp = (x - np.average(x))/np.std(x)
+#     f = fft2(xp)
+#     p = np.absolute(f)**2
+#     pi = ifft2(p)
+#     return np.real(pi)[:int(xp.shape[0]/2), :int(xp.shape[1]/2)]/(len(xp))
