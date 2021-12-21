147508677129f831d98b28fc8f3ba0d5f01c69bc
LudvigDillen <ludvig.dillen@gmail.com>
2021-12-21 20:13:43 +0100

    Add evaluate mir flickr data for hyperparameters

--- scripts/evaluate_mirflickr_all.py
+++ scripts/evaluate_mirflickr_all.py
@@ -14,6 +14,7 @@ python scripts/evaluate_mirflickr_admm.py \
 import glob
 import os
 import pathlib as plib
+import time
 from datetime import datetime
 
 import click
@@ -21,7 +22,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from diffcam.io import load_psf
 from diffcam.metric import lpips, mse, psnr, ssim
-from diffcam.mirflickr import postprocess, postprocess2
+from diffcam.mirflickr import postprocess
 from diffcam.plot import plot_image
 from diffcam.recon import Recon
 from diffcam.util import print_image_info
@@ -102,49 +103,128 @@ def mirflickr_dataset(data, n_files, n_iter, single_psf, save):
     psnr_scores = []
     ssim_scores = []
     lpips_scores = []
-    for fn in files:
-        bn = os.path.basename(fn).split(".")[0]
-        print(f"\n{bn}")
+    ####### CHANGE TO WHERE YOU WANT YOUR OUTPUT #######
+    local_dir = 'C:\\Users\\ludvi\\programming\\EPFL\\COM-514\\project_ours\\images\\mirflickr\\adrien\\'
+    ######### UNCOMMENT THE YOUR LINES OF CODE #########
+    ## Iskander's code
+    # modes = ['huber']
+    # lambdas = [1e-7, 1e-6, 1e-5, 1e-4,
+    #            1e-3, 1e-2, 1e-1]
+    # huber_delta = [0.5, 1.5, 2.5, 3.5, 4.5]
+    """ Iskander's command to run (stand in DiffuserCam when you run)
+    python scripts/evaluate_mirflickr_all.py --data subset_mir_flickr_dataset/nn
+    """
+    ## Elize's code
+    # modes = ['nn', 'dct']
+    # lambdas = [3e-7, 7e-7,
+    #            3e-6, 7e-6,
+    #            3e-5, 7e-5,
+    #            3e-4, 7e-4,
+    #            3e-3, 7e-3,
+    #            3e-2, 7e-2,
+    #            3e-1, 7e-1]
+    # huber_delta = [0]
+    """ Elize's command to run (stand in DiffuserCam when you run)
+    python scripts/evaluate_mirflickr_all.py --data subset_mir_flickr_dataset/nn
+    """
+    ## Ludvig's code
+    # modes = ['ridge']
+    # lambdas = [2e-8, 5e-8, 8e-8,
+    #            2e-7, 5e-7, 8e-7,
+    #            2e-6, 5e-6, 8e-6,
+    #            2e-5, 5e-5, 8e-5,
+    #            2e-4, 5e-4, 8e-4,
+    #            2e-3, 5e-3, 8e-3,
+    #            2e-2, 5e-2, 8e-2,
+    #            2e-1, 5e-1, 8e-1,
+    #            2,    5,    8]
+    # huber_delta = [0]
+    """ Ludvig's command to run (stand in DiffuserCam when you run)
+    python scripts/evaluate_mirflickr_all.py --data subset_mir_flickr_dataset/l2
+    """
+    ## Adrien's code
+    # modes = ['lasso', 'dct', 'nnL1']
+    # lambdas = [5e-8, 2e-7,
+    #            9e-7, 6e-6,
+    #            3e-5, 1e-4,
+    #            8e-4, 5e-3,
+    #            2e-2, 9e-2,
+    #            7e-1]
+    # huber_delta = [0]
+    """ Adrien's command to run (stand in DiffuserCam when you run)
+    python scripts/evaluate_mirflickr_all.py --data subset_mir_flickr_dataset/l1
+    """
+    ################################################
+    with open('metrics_flickrdata.txt', 'a') as f:
+        timestamp = datetime.now().strftime("%d-%m-%Y_%Hh%M")
+        f.write("\n\n" + timestamp + "\n")
+        for fn in files:
+            f.write("\n")
+            for looping_mode in modes:
+                f.write("\n")
+                for looping_lambda in lambdas:
+                    f.write("\n")
+                    for looping_delta in huber_delta:
+                        start_time = time.time()
+                        bn = os.path.basename(fn).split(".")[0]
+                        print(f"\n{bn}")
+                        # load diffuser data
+                        lensless_fp = os.path.join(diffuser_dir, fn)
+                        diffuser = np.load(lensless_fp)
+                        diffuser_prep = diffuser - background
+                        diffuser_prep = np.clip(diffuser_prep, a_min=0, a_max=1)
+                        diffuser_prep /= np.linalg.norm(diffuser_prep.ravel())
 
-        # load diffuser data
-        lensless_fp = os.path.join(diffuser_dir, fn)
-        diffuser = np.load(lensless_fp)
-        diffuser_prep = diffuser - background
-        diffuser_prep = np.clip(diffuser_prep, a_min=0, a_max=1)
-        diffuser_prep /= np.linalg.norm(diffuser_prep.ravel())
+                        solver = Recon(diffuser_prep, psf_float, mode=looping_mode, lambda1=looping_lambda, huber_delta = looping_delta)
+                        allout = solver.iterate()
+                        est = solver.get_estimate()
 
-        solver = Recon(diffuser_prep, psf_float, mode='ridge')
-        allout = solver.iterate()
-        est = solver.get_estimate()
+                        if save:
+                            np.save(os.path.join(save, f"{bn}.npy"), est)
+                            # viewable data
+                            output_fn = os.path.join(save, f"{bn}.tif")
+                            est_norm = est / est.max()
+                            image_data = (est_norm * 255).astype(np.uint8)
+                            im = Image.fromarray(image_data)
+                            im.save(output_fn)
 
-        if save:
-            np.save(os.path.join(save, f"{bn}.npy"), est)
-            # viewable data
-            output_fn = os.path.join(save, f"{bn}.tif")
-            est_norm = est / est.max()
-            image_data = (est_norm * 255).astype(np.uint8)
-            im = Image.fromarray(image_data)
-            im.save(output_fn)
+                        # compute scores
+                        lensed_fp = os.path.join(lensed_dir, fn)
+                        lensed = np.load(lensed_fp)
+                        lensed = postprocess(lensed)
+                        est = postprocess(est)
+                        plot_image(est)
+                        
+                        mse_scores.append(mse(lensed, est))
+                        psnr_scores.append(psnr(lensed, est))
+                        ssim_scores.append(ssim(lensed, est))
+                        lpips_scores.append(lpips(lensed, est))
+                        proc_time = time.time() - start_time
+                        # write metric data to txt file
+                        mse_data =  "MSE: " + str(mse_scores[-1])
+                        psnr_data = "PSNR: " + str(psnr_scores[-1])
+                        ssim_data = "SSIM: " + str(ssim_scores[-1])
+                        lpips_data = "LPIPS: " + str(lpips_scores[-1])
 
-        # compute scores
-        lensed_fp = os.path.join(lensed_dir, fn)
-        lensed = np.load(lensed_fp)
-        lensed = postprocess(lensed)
-        est = postprocess(est)
-        plot_image(est)
-        plt.show()
-        plot_image(lensed)
-        plt.show()
-        print("lensed: ", type(lensed), lensed.shape)
-        print("est: ", type(est), est.shape)
-        mse_scores.append(mse(lensed, est))
-        psnr_scores.append(psnr(lensed, est))
-        ssim_scores.append(ssim(lensed, est))
-        lpips_scores.append(lpips(lensed, est))
-        print(mse_scores[-1])
-        print(psnr_scores[-1])
-        print(ssim_scores[-1])
-        print(lpips_scores[-1])
+                        # handle extra parameter with Huber
+                        if looping_delta == 0:
+                            huber_txtstring = ""
+                            huber_filestring = ""
+                        else:
+                            f.write("\n")
+                            huber_txtstring = ", Huber delta: " + str(looping_delta)
+                            huber_filestring = '_huber_delta_' + str(looping_delta)
+
+                        explanatory_line = "File: " + bn + ", " + "mode: " + looping_mode + ", " + "lambda: " + \
+                                        str(looping_lambda) + huber_txtstring + ", " + "process time: " + str(proc_time)
+                        f.writelines([explanatory_line + "\n", mse_data + "\n", psnr_data + "\n",
+                                    ssim_data + "\n", lpips_data + "\n"])
+                        # save images in selected folder
+                        iteration_variant = bn + '_' + looping_mode + '_' +  str(looping_lambda) + huber_filestring + \
+                            '_proc_time_' + str(proc_time)
+                        iteration_variant = iteration_variant.replace('.', '_')
+                        plt.savefig(local_dir + iteration_variant + '.tiff')
+                        plt.close('all')
 
     if save:
         print(f"\nReconstructions saved to : {save}")
